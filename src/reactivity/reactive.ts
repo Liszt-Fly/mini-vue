c3e6560d00bba54a68fc27a6cb359b11f479d6a9
Liszt-Fly <1904231450@qq.com>
2022-08-15 16:41:41 +0800

    update: effect 和 reactive

---
+++ src/reactivity/reactive.ts
@@ -0,0 +1,18 @@
+import { track } from "./effect"
+
+export function reactive(raw) {
+    return new Proxy(raw, {
+        get(target, key) {
+            const res = Reflect.get(target, key)
+            //TODO 执行依赖收集
+            track(target, key)
+            return res
+        },
+
+        set(target, key, value) {
+            const res = Reflect.set(target, key, value)
+            //TODO 执行依赖触发
+            return res
+        }
+    })
+}
